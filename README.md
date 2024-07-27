91fcc869685066adcd0676ff26992269447ff594
Adem Aydin <mail@adem.dev>
2024-07-28 00:42:09 +0200

    Provide basic web UI; adjust README

--- README.md
+++ README.md
@@ -14,10 +14,14 @@ curl -X POST http://localhost:8000/api/snomed-ct \
      -H 'Content-Type: application/sparql-query' \
      -d @examples/ct-of-thorax-synonyms.sparql
 ```
+- Perform queries via the [web UI](http://localhost:3000).
 
 # Caveats
-- The input data provided in `data/snomed-ct.csv` is a very small subset of the file `sct2_Description_Full-en_INT_20240501.txt` terminology dataset provided in the SNOMED CT International Edition release. The full version can be obtained at https://mlds.ihtsdotools.org via an MLDS license.
+The input data provided in `data/snomed-ct.csv` is a very small subset of the file `sct2_Description_Full-en_INT_20240501.txt` terminology dataset, converted to be comma-separated, provided in the SNOMED CT International Edition release.
+
+The full version can be obtained at https://mlds.ihtsdotools.org via an MLDS license. You may place the full version of the file in `data/snomed-ct.csv` instead.
 
 # To-dos
+- [ ] Extend RML mapping to more fields such as description
 - [ ] Investigate [snomed-owl-toolkit](https://github.com/IHTSDO/snomed-owl-toolkit)
-- [ ] Provide a rudimentary web interface
+- [x] Provide a basic web UI
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,45 @@
+import { useState } from 'react'
+
+const sparqlQuery = (keyword: string) => `\
+PREFIX ex: <http://example.com/ns#>
+SELECT ?term WHERE {
+	?subject ex:term ?inputTerm .
+	FILTER(CONTAINS(LCASE(?inputTerm), LCASE("${keyword}")))
+	?subject ex:conceptId ?conceptId .
+	?otherSubject ex:conceptId ?conceptId .
+	?otherSubject ex:term ?term .
+}
+LIMIT 10`
+
+const makeRequest = (payload: string): Promise<unknown> => {
+  return fetch('http://localhost:8000/api/snomed-ct', {
+    method: 'POST',
+    body: payload,
+    headers: { 'Content-Type': 'application/sparql-query' },
+  }).then((response) => response.json())
+}
+
 export function App() {
-  return <>Hello, SNOMED CT!</>
+  const [response, setResponse] = useState<unknown | null>(null)
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    // TODO: Cancel ongoing request for new input.
+    if (event.target.value.length === 0) {
+      setResponse(null)
+    } else {
+      makeRequest(sparqlQuery(event.target.value)).then(setResponse)
+    }
+  }
+
+  return (
+    <div className="App">
+      <input
+        placeholder="e.g. &ldquo;Thorax&rdquo;&hellip;"
+        onChange={handleChange}
+      />
+      {response !== null && (
+        <pre className="response">{JSON.stringify(response, null, 2)}</pre>
+      )}
+    </div>
+  )
 }
